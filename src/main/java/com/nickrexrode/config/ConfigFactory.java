1f0351aa0733c6697db1f594205f9f6231f49df7
nickrexrode <nrexrode02@gmail.com>
2021-08-02 17:50:38 -0500

    created factory to load an ApplicationConfig as Config

---
+++ src/main/java/com/nickrexrode/config/ConfigFactory.java
@@ -0,0 +1,24 @@
+package com.nickrexrode.config;
+
+import com.nickrexrode.exception.config.ConfigNotFoundException;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+public final class ConfigFactory {
+
+    public static ApplicationConfig loadConfiguration(File file) {
+        Yaml yaml = new Yaml();
+        Map<String, Object> map = null;
+        try {
+            map = yaml.load(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            throw new ConfigNotFoundException();
+        }
+
+        return new ApplicationConfig((String) map.get("application"),map, file);
+    }
+}
