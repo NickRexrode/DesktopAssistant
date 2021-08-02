71fec299b9c512fcd92874f0dca29caf67540abd
nickrexrode <nrexrode02@gmail.com>
2021-08-02 17:54:50 -0500

    updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,12 @@ module com.nickrexrode.DesktopAssistant {
     requires javafx.graphics;
     requires javafx.base;
     requires org.yaml.snakeyaml;
+    requires com.google.gson;
+
 
     opens com.nickrexrode to javafx.fxml;
     opens com.nickrexrode.gui to javafx.fxml;
+    opens com.nickrexrode.gui.loader to javafx.fxml;
     exports com.nickrexrode;
 
 }
