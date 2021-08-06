6ead58916bf3e4d24913c594c9909d9069a17dc7
nickrexrode <nrexrode02@gmail.com>
2021-08-05 22:43:46 -0500

    Formatting

--- src/main/java/com/nickrexrode/web/request/TestRequest.java
+++ src/main/java/com/nickrexrode/web/request/TestRequest.java
@@ -5,7 +5,7 @@ import com.nickrexrode.web.response.TestResponse;
 public final class TestRequest extends Request<TestResponse> {
 
     public TestRequest() {
-        super("https://americas.api.riotgames.com/lol/match/v5/matches/NA1_3885044733?api_key=RGAPI-585d9107-cc50-44ff-8ff5-ed75e5b62c4b");
+        super("https://americas.api.riotgames.com/lol/match/v5/matches/NA1_3885044733?api_key=apikey");
     }
 
     @Override
